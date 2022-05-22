73ba3c30da292dfb792312052726ee9f1e06d3ef
PrashantaPH <prashantpl8722@gmail.com>
2022-05-22 15:21:07 +0530

    updated

--- src/main/java/com/ty/pack/Vaibhav.java
+++ src/main/java/com/ty/pack/Vaibhav.java
@@ -4,5 +4,8 @@ public class Vaibhav {
 	public static void main(String[] args) {
 		System.out.println("hiiiiii");
 	}
-
+	public static void m1()
+	{
+		System.out.println("hey vaibyyyaaa");
+	}
 }
